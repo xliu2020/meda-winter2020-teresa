5c0ce6c70e4025262bfb2c750019613d619905ee
Your Name <you@example.com>
2020-11-17 16:48:20 +0800

    adding revision of todo

--- projects/todo/argumentsaver.js
+++ projects/todo/argumentsaver.js
@@ -14,7 +14,7 @@ if (fileExist) {
     contents = fs.readFileSync("example.json", "utf-8");
     contentsObject = JSON.parse(contents);
 } else {
-    console.log("sorry, the file doesn't exit. create a example.json now")
+  
     objectConverted=JSON.stringify(myObject);
      console.log(objectConverted); //why object can be converted to json sucessfully only after adding console.log
     fs.writeFileSync("example.json", objectConverted, "utf-8");
@@ -24,14 +24,16 @@ if (fileExist) {
 
 let addedcontent= arguments[2];
 
-if (addedcontent) {
- 
-    contentsObject[contentsObject.length-1]=addedcontent;
-    contentsObject = JSON.stringify(contentsObject);
-    // console.log(contentsObject);
-    fs.writeFileSync("example.json",contentsObject,"utf-8");
 
-} else {
+    if (addedcontent) {
+        contentsObject[Math.random()]=[addedcontent];   
+        contentsObject = JSON.stringify(contentsObject);
+        console.log(contentsObject);
+        fs.writeFileSync("example.json",contentsObject,"utf-8");
+    }
+   
+
+ else{
 
     console.log("There was no argument found.") ;
 
