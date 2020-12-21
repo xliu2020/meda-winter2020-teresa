4406ffb6e006b0118748c9d69a9be9519a2178d7
Your Name <you@example.com>
2020-12-21 14:35:53 +0800

    pushing todo app

--- assignments/week7/todo/client/scripts/main.js
+++ assignments/week7/todo/client/scripts/main.js
@@ -1,3 +1,107 @@
+const base_url = "http://localhost:3000/";
+
 $(document).ready(() =>{
-console.log("Hello");
-})
+
+    $.post(base_url + "getlist", {}, (data) => {
+        console.log(data.list);
+
+        for (let i =0; i < data.list.length; i++) {
+                addToList(data.list[i]);
+        }
+
+        refreshDeleteButtons()
+        
+        
+    });
+
+    $("#submit").click(() => {
+
+        let priorityNumber = $("#note-priority option:selected").attr("value");
+
+        priorityNumber = parseInt(priorityNumber);
+
+        let noteObject = {
+            author:null,
+            title:$("#note-title").val(),
+            text:$("#note-text").val(),
+            completed:false,
+            archieved:false,
+            priority:priorityNumber,
+            cateogries:null,
+            timestamp:Date.now()
+        }
+
+        $("#submit").attr("disabled", "disabled");
+        $("#loading-icon").fadeIn(600);
+   
+
+        $.post(base_url +"newNote", noteObject, (data) => {
+            console.log( data.saved);
+            if(data.saved === true) {
+                setTimeout(() => {
+                    $("#submit").removeAttr("disabled");
+                    $("#loading-icon").fadeOut(600);
+                    $("#submit-message").text("Successfully saved " + noteObject.title);
+                    $("#submit-message").fadeIn(600);
+
+                    addToList(data.savedTask);
+                    refreshDeleteButtons();
+
+                    setTimeout(() => {
+                        $("#submit-message").fadeOut(600, () => {
+                            $("submit-message").text("");
+                            $("#note-title").val("");
+                            $("#note-text").val("");
+                        });
+
+                    }, 4000);
+                },1200);
+            }
+       });
+
+         
+    });
+});           
+
+
+function addToList(taskObject) {
+    const html = `
+        <tr data-task-id = "${taskObject._id}">
+            <td>${taskObject.priority}</td>
+            <td>${taskObject.title}</td>
+            <td>${taskObject.text}</td>
+            <td><button>Delete</button></td>
+            <td><img src="images/822.gif" width="22" height="22" alt = "loading icon" /></td>
+        </tr>
+    `;
+
+    $("#tasks tbody").append(html);
+
+}
+
+function refreshDeleteButtons() {
+
+    $("#tasks tbody tr td button").off();
+
+    $("#tasks tbody tr td button").click(function () {
+
+        $(this).attr("disabled", "disabled");
+
+        $(this).parent().next().children("img").fadeIn(600);
+
+        let taskID = $(this).parent().parent().attr("data-task-id");
+
+        let actionObject = {
+            id:taskID,
+            action:"delete",
+            data:null
+        }
+
+        $.post(base_url + "modify", actionObject, (data) => {
+            setTimeout(() => {
+                $(this).parent().parent().fadeOut(600);
+            },1200);
+            
+        });
+    })
+}
--- assignments/week7/todo/index.js
+++ assignments/week7/todo/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const { connect } = require("http2");
+//  const connect = require("http2");
+const credential = require('./credential.js');
+const { parse } = require("path");
+const { response } = require("express");
+// const { stringify } = require("querystring");
 
-const dbCredentials = "mongodb+srv://todouser:todouser@cluster0.oqdfn.mongodb.net/todoapp?retryWrites=true&w=majority;"
-const options={useNewUrlParser:true, useFindAndModify:false, useUnifiedTopology: true};
+const dbCredentials = credential.dbURL;
 
-let dbConnection = mongoose.connect(dbCredentials,options, (error) => {
+const dbOptions={useNewUrlParser:true, useFindAndModify:false, useUnifiedTopology: true};
+
+let dbConnection = mongoose.connect(dbCredentials, dbOptions, (error) => {
     if(error) {
         console.log("Mongoose error:" + error);
     } else { 
@@ -14,6 +19,16 @@ let dbConnection = mongoose.connect(dbCredentials,options, (error) => {
     }
 })
 
+let todoSchema = new mongoose.Schema({
+    title: String,
+    text:String,
+    completed:Boolean,
+    archived:Boolean,
+    priority:Number,
+    timestamp:Number
+});
+
+let todoModel = new mongoose.model("notecollections", todoSchema);
 
 const port = 3000;
 
@@ -27,3 +42,71 @@ app.use(bodyParser.urlencoded({extended: false}));
 console.log("Express server is running on port" + port);
 
 app.use("/", express.static("client/"));
+
+app.post("/newNote", (request,response) => {
+    const note = request.body;
+
+//    console.log(note);
+
+    // sanitize inputs
+
+    let newNoteDocument = new todoModel({
+        title:note.title,
+        text:note.text,
+        completed:(note.completed === "true"),
+        archived:(note.archived === "true"),
+        priority:parseInt(note.priority),
+        timestamp:parseInt(note.timestamp)
+    });
+
+    newNoteDocument.save((error) => {
+
+        const responseObject = {
+            saved:false,
+            savedTask:newNoteDocument,
+            error:null
+        };
+
+        if (error) {
+            response.send(responseObject);
+        } else {
+            responseObject.saved = true;
+            response.send(responseObject);
+        }
+    });
+});
+
+app.post("/getList", (request,response) => {
+    todoModel.find({}, (error, results) => {
+
+        const responseObject = {
+            list:results,
+            error:null
+        };
+
+        if (error) {
+            console.log("failed to read database.");
+        } else {
+            response.send(responseObject);
+        }
+    });
+});
+
+app.post("/modify", (req, res) => {
+
+    let request = req.body;
+
+    if (request.action === "delete") {
+        todoModel.findByIdAndDelete(request.id, (error,deleted) => {
+            if (error) {
+                console.log(error);
+            } else {
+                let response = {
+                    copy:deleted
+                }
+
+                res.send(response);
+            }
+        })
+    }
+})
